83ab67be00128f40b68b7fc58a90e0e0513cd755
Ravish Bisht <ravishbisht85@gmail.com>
2024-10-02 16:41:50 +0530

    adding button to visit sweet note route

--- src/app/add-notes/page.tsx
+++ src/app/add-notes/page.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import CloudinaryUploadBtn from "@/components/CloudinaryUploadBtn";
 import AudioRecorder from "@/components/AudioRecorder";
 import { MdCleaningServices } from "react-icons/md";
+import Link from "next/link";
 
 export default function Notes() {
   const [messageNote, setMessageNote] = useState<string>("");
@@ -103,6 +104,15 @@ export default function Notes() {
                 {loading ? "loading..." : "Submit"}
               </span>
             </button>
+            <Link href="/show-notes">
+              <button
+                className={`relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800`}
+              >
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  see sweet notes
+                </span>
+              </button>
+            </Link>
           </div>
         </div>
       </div>
